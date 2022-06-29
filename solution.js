945751188b10160a4d9e7bc10aa225654cec26d1
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-06-29 15:03:25 +0200

    ex with str and others

--- solution.js
+++ solution.js
@@ -1,1 +1,24 @@
+const str = "izabela";
+
+console.log(str[1], str[5]);
+
+let firstName = "Izabela from Poland";
+let age = 35;
+let year = 1987;
+console.log(firstName,`is`, age,  `years old`,`who lived in`, year);
+
+let isMarried = false;
+console.log(firstName, `is married`, isMarried);
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage, `is fun:`, isFun);
+
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+console.log(programmingLanguage = programmingLanguage.length) ;
+
+
+
+
 
